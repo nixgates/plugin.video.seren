6d6b33bc956943142269c8b282c23c74483a1442
nixgates <s>
2018-11-26 15:30:22 +0930

    Version 0.1.10
    Adjusted Migration Script so it no longer broke Super Faviorates
    Adjusted TMDB movie artwork to fix issue where it would display the wrong media (removed thumbnail)

--- service.py
+++ service.py
@@ -6,40 +6,14 @@ tools.log('##################  STARTING SERVICE  ######################')
 tools.log('Checking for incorrect addon ID')
 import os, xbmc
 
-
-if tools.kodiVersion == 17:
-    if tools.condVisibility('System.HasAddon(plugin.video.Seren)'):
-        tools.log('Incorrect Addon ID Identified')
-        tools.log('Performing Migration')
-        data_path = os.path.join(xbmc.translatePath('special://home'), 'userdata', 'addon_data')
-        dir_listing = os.listdir(data_path)
-        if 'plugin.video.Seren' in dir_listing:
-            os.rename(os.path.join(data_path, 'plugin.video.Seren'), os.path.join(data_path, 'plugin.video.seren'))
-
-        platform = sys.platform
-
-        tools.showDialog.yesno('Seren',
-                               'Because of an issue with original addon ID on release,'
-                               ' Seren requires a restart of Kodi after this update.\n'
-                               'Please restart Kodi now.')
-    else:
-        tools.log('No old release version found on device')
-
-if tools.kodiVersion == 18:
-    tools.log("VERSION 18")
-if tools.kodiVersion == '18':
-    tools.log("TEXT VERSION 18")
-
-if tools.kodiVersion == 18:
-    tools.log('VERSION 18 FOUND')
-    data_path = os.path.join(xbmc.translatePath('special://home'), 'userdata', 'addon_data')
-    dir_listing = os.listdir(data_path)
-    if 'plugin.video.Seren' in dir_listing:
-        os.rename(os.path.join(data_path, 'plugin.video.Seren'), os.path.join(data_path, 'plugin.video.seren'))
-        tools.showDialog.yesno('Seren',
-                               'Because of an issue with original addon ID on release,'
-                               ' Seren requires a restart of Kodi after this update.\n'
-                               'Please restart Kodi now.')
+data_path = os.path.join(xbmc.translatePath('special://home'), 'userdata', 'addon_data')
+dir_listing = os.listdir(data_path)
+if 'plugin.video.Seren' in dir_listing:
+    os.rename(os.path.join(data_path, 'plugin.video.Seren'), os.path.join(data_path, 'plugin.video.seren'))
+    tools.showDialog.ok('Seren',
+                        'Because of an issue with original addon ID on release,'
+                        ' Seren requires a restart of Kodi after this update.\n'
+                        'Please restart Kodi now.')
 
 tools.log('Checking Common API Tokens for refresh')
 maintenance.run_maintenance()
