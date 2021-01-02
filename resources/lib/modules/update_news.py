35566210bb395834278a49e6aedaa878d5e94487
Nixgates <->
2021-01-02 16:16:55 +0930

    Added default value of None for skin color when attempting to pull color from user's skin Add missing update_news module

---
+++ resources/lib/modules/update_news.py
@@ -0,0 +1,18 @@
+import xbmcgui
+
+from resources.lib.modules.globals import g
+
+last_update_required = "2.0"
+
+update_messages = [
+        g.get_language_string(30573)
+    ]
+
+def do_update_news():
+    if last_update_required == g.get_setting("update.news.version"):
+        return
+
+    for msg in update_messages:
+        xbmcgui.Dialog().ok(g.ADDON_NAME, msg)
+
+    g.set_setting("update.news.version", last_update_required)
