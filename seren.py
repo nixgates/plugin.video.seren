157db3eb014fedd2490b31a938c34d162627c4b9
henryjfry <44173765+henryjfry@users.noreply.github.com>
2021-08-05 15:11:11 +0100

    commented out modification
    commented out modification

--- seren.py
+++ seren.py
@@ -20,3 +20,62 @@ try:
         router.dispatch(g.REQUEST_PARAMS)
 finally:
     g.deinit()
+#xbmc.log(str('SEREN_MODIFICATION')+'===>PHIL', level=xbmc.LOGINFO)
+"""
+try:
+    curr_time = time.time()
+    with TimeLogger('{}'.format(g.REQUEST_PARAMS.get('action', ''))):
+        router.dispatch(g.REQUEST_PARAMS)
+    while time.time() - curr_time < 3:
+        #xbmc.log(str(1)+'===>PHIL', level=xbmc.LOGINFO)
+        xbmc.sleep(1000)
+        #xbmc.log(str(2)+'===>PHIL', level=xbmc.LOGINFO)
+        if not xbmc.Player().isPlaying() and not xbmc.getCondVisibility('Window.IsActive(persistent_background.xml)'):
+            #xbmc.log(str(3)+'===>PHIL', level=xbmc.LOGINFO)
+            #xbmc.log(str(g.REQUEST_PARAMS['smartPlay'])+'===>PHIL', level=xbmc.LOGINFO)
+            #xbmc.log(str(g.REQUEST_PARAMS['source_select'])+'===>PHIL', level=xbmc.LOGINFO)
+            #del router
+            try:
+                test_var = str(g.REQUEST_PARAMS['source_select']) == 'true' or str(g.REQUEST_PARAMS['smartPlay']) == 'true'
+            except:
+                #g.deinit()
+                #g.deinit()
+                #g.deinit()
+                del router
+                del g
+                del TimeLogger
+                exit()
+                #break
+            if str(g.REQUEST_PARAMS['source_select']) == 'true' or str(g.REQUEST_PARAMS['smartPlay']) == 'true':
+                router.dispatch(g.REQUEST_PARAMS)
+                xbmc.sleep(1000)
+                #g.deinit()
+                #g.deinit()
+                #g.deinit()
+                del router
+                del g
+                del TimeLogger
+                exit()
+            else:
+                #g.deinit()
+                #g.deinit()
+                #g.deinit()
+                del router
+                del g
+                del TimeLogger
+                exit()
+finally:
+    try:
+        g.deinit()
+    except:
+        pass
+try:
+    g.deinit()
+    g.deinit()
+    del router
+    del g
+    del TimeLogger
+except:
+    pass
+exit()
+"""
